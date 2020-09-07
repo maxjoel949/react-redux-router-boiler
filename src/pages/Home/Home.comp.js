d394051097587de6a28af1bc896a3f1e71e85bfc
Max <max.joel949@gmail.com>
2020-09-07 01:18:16 -0300

    added useSelector example

--- src/pages/Home/Home.comp.js
+++ src/pages/Home/Home.comp.js
@@ -1,12 +1,15 @@
 import React from 'react'
+import { useSelector } from 'react-redux'
 import './Home.scss'
 
 import { Link } from 'react-router-dom'
 
 const Home = (props) => {
+    console.log(useSelector(state => state))
+    const hola = useSelector(state => state.HomeReducer.hola)
     return (
         <div className="home__wrapper">
-            <h1> Soy el home </h1>
+            <h1> Soy el home { hola } </h1>
             <Link to="/admin/setting">La setting prro</Link>
         </div>
     )
